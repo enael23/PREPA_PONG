3ca71168dc2002aca8c4d010930caa6cf6900818
Jordan PAULINE <jpauline@made-f0Br6s18.clusters.42paris.fr>
2023-09-25 22:02:48 +0200

    avancee pong keydown, keyup et @Interval

--- NESTJS-SOCKETIO/app.module.ts
+++ NESTJS-SOCKETIO/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketModule } from './socket/socket.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [SocketModule],
+  imports: [SocketModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- NESTJS-SOCKETIO/socket/socketEvents.ts
+++ NESTJS-SOCKETIO/socket/socketEvents.ts
@@ -1,4 +1,6 @@
+import { Interval } from "@nestjs/schedule";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { interval } from "rxjs";
 import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway({cors: {origin: '*',},})
@@ -23,5 +25,56 @@ export class SocketEvents {
         // envoyer un event
         console.log(`Message : ${data}`);
         this.server.emit('message', client.id, data);
+    }
+
+    pos: number = 100;
+    balR: number = 10;
+    balPosX: number = 200;
+    balPosY: number = 200;
+    balVelX: number = 5;
+    balVelY: number = 10;
+
+    paddleKeyUp: boolean = false;
+    paddleKeyDown: boolean = false;
+
+    tic: number = 0;
+
+    @SubscribeMessage('keyPress')
+    handleKeyPress(@MessageBody() key: string){
+        console.log(`Key pressed : ${key}`);
+        // if (key == 'ArrowUp'){this.pos -= 10;}
+        // if (key == 'ArrowDown'){this.pos += 10;}
+        // this.server.emit('key', this.pos);
+        // this.server.emit('key', this.pos, this.balPosX, this.balPosY);
+        if (key == 'ArrowUp'){this.paddleKeyUp = true;}
+        if (key == 'ArrowDown'){this.paddleKeyDown = true;}
+    }
+
+    @SubscribeMessage('keyRelease')
+    handleKeyRelease(@MessageBody() key: string){
+        console.log(`Key released: ${key}`);
+        // if (key == 'ArrowUp'){this.pos -= 10;}
+        // if (key == 'ArrowDown'){this.pos += 10;}
+        // this.server.emit('key', this.pos);
+        // this.server.emit('key', this.pos, this.balPosX, this.balPosY);
+        if (key == 'ArrowUp'){this.paddleKeyUp = false;}
+        if (key == 'ArrowDown'){this.paddleKeyDown = false;}
+    }
+
+
+    @Interval(15)
+    handleInterval(){
+        if (this.balPosX < this.balR || this.balPosX > 400 - this.balR)
+            this.balVelX = -this.balVelX;
+        if (this.balPosY < this.balR || this.balPosY > 400 - this.balR)
+            this.balVelY = -this.balVelY;
+        this.balPosX += this.balVelX;
+        this.balPosY += this.balVelY;
+
+        if (this.paddleKeyDown){this.pos += 20;}
+        if (this.paddleKeyUp){this.pos -= 20;}
+
+        this.server.emit('key', this.pos, this.balPosX, this.balPosY);
+        // console.log(`tic ${this.tic++}`);
     }
 }
